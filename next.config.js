94d92915118d4744612a877ceb5d8f80e72a22a7
Rockefeller <rockefeller92@protonmail.com>
2021-02-17 13:05:06 -0800

    added missing abi for SynthetixDelegateApprovals updated next.config.js to hopefully fix issues with IPFS deployment on fleek

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+const optimizedImages = require('next-optimized-images');
+
+// all the dynamic pages need to be defined here (this needs to be imported from the routes)
+
+module.exports = {
+	...optimizedImages,
+	exportTrailingSlash: true,
+	exportPathMap: function() {
+		return {
+			'/': { page: '/' }
+		};
+	}
+}
+
