a6e5eb6c5da21238cbb90953e169253eb41702f3
Rockefeller <rockefeller92@protonmail.com>
2021-02-17 22:03:28 -0800

    various project config and package tweaks inline svg imports are working

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 // next.config.js
+const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 // all the dynamic pages need to be defined here (this needs to be imported from the routes)
-
-module.exports = {
-	...optimizedImages,
-	exportTrailingSlash: true,
-	exportPathMap: function() {
+module.exports = withPlugins([[optimizedImages]], {
+	trailingSlash: !!process.env.NEXT_PUBLIC_TRAILING_SLASH_ENABLED,
+	exportPathMap: function (defaultPathMap) {
 		return {
-			'/': { page: '/' }
+			...defaultPathMap
 		};
-	}
-}
-
+	},
+});
