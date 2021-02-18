50068dc055fe69f003139255aa32afb890476917
Rockefeller <rockefeller92@protonmail.com>
2021-02-17 22:19:59 -0800

    trying to fix some paths so that ipfs hosted site works

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const optimizedImages = require('next-optimized-images');
 
 // all the dynamic pages need to be defined here (this needs to be imported from the routes)
 module.exports = withPlugins([[optimizedImages]], {
-	trailingSlash: !!process.env.NEXT_PUBLIC_TRAILING_SLASH_ENABLED,
+	trailingSlash: true,//!!process.env.NEXT_PUBLIC_TRAILING_SLASH_ENABLED,
 	exportPathMap: function (defaultPathMap) {
 		return {
 			...defaultPathMap
